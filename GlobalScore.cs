3beb5bfab29b0583e956b22bd98920891be05fc8
Zixun <68407964+Pax636@users.noreply.github.com>
2020-10-30 17:45:53 -0400

    Add files via upload
    All three files should be in one empty object for every scene, whenever an other class wants to use one of them, that other class define a class variable and then use the methods inside.
    Updater: Storing all the data the UI needs, including the value of Text on the screen, the current health value of everyone.
    Manager: Essentially, a tool box for using the methods in Updater without defining the Text Object. (Should be used by every character).
    GlobalScore: The only class that will store data cross different scene. Use the method inside through Manager class before switching scene.

---
+++ GlobalScore.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalScore : MonoBehaviour
+{
+    public static GlobalScore instance;
+
+    private float totalDemegePartyToBoss_Level1;
+    private float totalDemageBossToParty_Level1;
+
+    private float totalDemegePartyToBoss_Level2;
+    private float totalDemageBossToParty_Level2;
+
+    private float totalDemegePartyToBoss_Level3;
+    private float totalDemageBossToParty_Level3;
+
+    //The following Awake() is to ensure that there is only one piece of data when switching between scenes.
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this) 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Update to corresponding level if the new record is greater
+
+    public void CheckAndUpdateScoreClass(float totalP2B, float totalB2P) 
+    {
+        //Find the current Scene
+        if (SceneManager.GetActiveScene().name.Equals("Level1"))
+        {
+            //Check if it is greater for both value of a level
+            if (totalP2B > totalDemegePartyToBoss_Level1) 
+            {
+                totalDemegePartyToBoss_Level1 = totalP2B;
+            }
+            if (totalB2P > totalDemageBossToParty_Level1) 
+            {
+                totalDemageBossToParty_Level1 = totalB2P;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name.Equals("Level2"))
+        {
+            if (totalP2B > totalDemegePartyToBoss_Level2)
+            {
+                totalDemegePartyToBoss_Level2 = totalP2B;
+            }
+            if (totalB2P > totalDemageBossToParty_Level2)
+            {
+                totalDemageBossToParty_Level2 = totalB2P;
+            }
+        }
+        else if (SceneManager.GetActiveScene().name.Equals("Level3"))
+        {
+            if (totalP2B > totalDemegePartyToBoss_Level3)
+            {
+                totalDemegePartyToBoss_Level3 = totalP2B;
+            }
+            if (totalB2P > totalDemageBossToParty_Level3)
+            {
+                totalDemageBossToParty_Level3 = totalB2P;
+            }
+        }
+        else 
+        {
+            Debug.Log("Cannot match the scene");
+        }
+        
+    }
+
+
+}
